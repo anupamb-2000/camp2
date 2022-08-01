e606d2c874774eda1a718dc6bef14f4fe423b53b
anupam <anupam.b@inapp.com>
2022-08-01 16:14:11 +0530

    Added changes to q1

--- Product Management System/products.py
+++ Product Management System/products.py
@@ -1,16 +1,29 @@
 class Product:
     products = []
+
+    def getRandomNumber(self):
+        return str(123)
+    
+    def getProductCount(self, name, category):
+        count = 0
+        for product in Product.products:
+            if (product["Name"] == name and product["category"] == category):
+                count += 1
+        return str(count)
+
+    def temp(self):
+        print(self.name)
+        print(self.product_code)
+
     def __init__(self, name, category, basic_price, tax, discount, MRP):
         self.name = name.upper()
         self.category = category.upper()
-        self.product_code = name[0:2] + category[0:2] + getProductCount(name, category) + getRandomNumber()
+        self.product_code = self.name[0:2] + self.category[0:2] + self.getProductCount(name, category) + self.getRandomNumber()
         self.basic_price = basic_price
         self.tax = tax
         self.discount = discount
         self.MRP = MRP
 
-    def getProductCount(self, name, category):
-        
 
 while(True):
     print("Menu : ")
@@ -22,8 +35,15 @@ while(True):
         case 1:
             pass
         case 2:
-            pass
+            name = input("Enter name of the product : ")
+            category = input("Enter category name : ")
+            basic_price = float(input("Enter basic price : "))
+            tax = float(input("Enter tax : "))
+            discount = float(input("Enter discount : "))
+            MRP = float(input("Enter MRP : "))
+            pro = Product(name, category, basic_price, tax, discount, MRP)
+            pro.temp()
         case 3:
-            pass
+            exit()
         case _:
             print("Invalid input!")
